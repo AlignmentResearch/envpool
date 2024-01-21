ae6a2d7f481367de6a63cc990dd7f9f50729f1c4
Adrià Garriga-Alonso <adria@far.ai>
2024-01-21 11:50:48 -0800

    Print reward boxes

--- envpool/sokoban/sokoban_envpool.cc
+++ envpool/sokoban/sokoban_envpool.cc
@@ -3,6 +3,7 @@
 #include <array>
 #include <sstream>
 #include <stdexcept>
+#include <iostream>
 
 #include "envpool/core/py_envpool.h"
 
@@ -149,7 +150,10 @@ void SokobanEnv::Step(const Action& action_) {
   const double reward =
       reward_step +
       reward_box * static_cast<double>(prev_unmatched_boxes - unmatched_boxes) +
-      ((unmatched_boxes == 0) ? reward_finished : 0.0f);
+      ((unmatched_boxes == 0) ? reward_finished : 0.0);
+  std::cout << "prev_unmatched_boxes=" << prev_unmatched_boxes
+            << ", unmatched_boxes=" << unmatched_boxes
+            << ", so reward=" << reward << "\n";
   WriteState(static_cast<float>(reward));
 }
 
